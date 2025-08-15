2907ea46306871d2a8d016d62ab2f93f993d9fb6
secretsixes <121305420+secretsixes@users.noreply.github.com>
2025-08-15 19:53:52 +0100

    Update keyboard_input_manager.js

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -49,7 +49,7 @@ KeyboardInputManager.prototype.listen = function () {
         self.emit("move", mapped);
       }
 
-      if (event.which === 88) self.restart.bind(self)(event);
+      if (event.which === 32) self.restart.bind(self)(event);
     }
   });
 
